e9f935ea97c878dda8fb9326c8664702ce4a011f
choiruladamm <choiruladamm@gmail.com>
2023-02-28 10:49:26 +0700

    course w3schools

---
+++ Course-w3shools/01-JS-Tutorial/10-string-methods.js
@@ -0,0 +1,29 @@
+// * JS STRING METHOD
+
+// * length
+{
+  // let abjad = "jsdjksbdksbdkjsbdjksd";
+  // let length = abjad.length;
+  // console.log(length);
+}
+
+// * slice
+{
+  // let text = "Apple, Banana, Kiwi";
+  // let part = text.slice(-12, -6);
+  // console.log(part);
+}
+
+// * substring()
+{
+  // let text = "Apple, Banana, Kiwi";
+  // let part = text.substring(7,13);
+  // console.log(part);
+}
+
+// * split()
+{
+ let text = "1,b,c,d,e,f,g,h,i"
+ const myArray = text.split("")
+ console.log(myArray)
+}
