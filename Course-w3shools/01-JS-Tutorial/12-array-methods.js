5ce94f21ecafbb00fc75ff22ff12f4807e104a7e
choiruladamm <choiruladamm@gmail.com>
2023-02-28 14:10:48 +0700

    course w3schools

---
+++ Course-w3shools/01-JS-Tutorial/12-array-methods.js
@@ -0,0 +1,38 @@
+// * Convert Array to String
+// const fruits = ["banan", "apple", "orange", "durian"];
+
+// console.log(fruits.join(" * "));
+
+// * Array pop()
+// const fruits = ["banana", "apple", "orange", "durian"];
+// console.log(fruits.pop());
+// fruits.pop();
+
+// * Array push()
+// const fruits = ["banana", "apple", "orange", "durian"];
+// let length = fruits.push("rambutane")
+
+// console.log(fruits)
+
+// * Array shift()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits.shift();
+
+// console.log(fruit)
+
+// * Array unshift()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Lemon");
+// console.log(fruits)
+
+// * Array length
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits[fruits.length] = "Kiwi";
+// console.log(fruits);
+
+// * Array length
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log(myChildren);
