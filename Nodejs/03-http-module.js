2244c71dcfa6becad7c8b4cd92970d63094d2fcd
choiruladamm <choiruladamm@gmail.com>
2023-03-11 13:57:11 +0700

    course nodejs

---
+++ Nodejs/03-http-module.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+// create a server object
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" }); // Add an HTTP Header
+    res.write(req.url); // Read the Query String
+    res.end(); // end the response
+  })
+  .listen(8080); // the server object listens on port 8080
