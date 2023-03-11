43be3892cff370fb2a9e506fea6f448401e9a19d
choiruladamm <choiruladamm@gmail.com>
2023-03-11 15:02:19 +0700

    course nodejs

--- Nodejs/02-module-demo.js
+++ Nodejs/02-module-demo.js
@@ -4,7 +4,7 @@ var dt = require("./02-module");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write("Tanggal dan Wahtu sekarang: " + dt.myDateTime());
     res.end();
   })
   .listen(8080);
--- Nodejs/03-http-module.js
+++ Nodejs/03-http-module.js
@@ -1,10 +1,13 @@
 var http = require("http");
-
+var url = require("url");
 // create a server object
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" }); // Add an HTTP Header
-    res.write(req.url); // Read the Query String
-    res.end(); // end the response
+    // res.write(req.url); // Read the Query String
+    // res.end(); // end the response
+    var q = url.parse(req.url, true).query; // parse the query string
+    var txt = q.year + " " + q.month;
+    res.end(txt);
   })
   .listen(8080); // the server object listens on port 8080
