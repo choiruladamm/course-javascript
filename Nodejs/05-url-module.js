0dd21645286723106397c18c295fe61bdce98629
choiruladamm <choiruladamm@gmail.com>
2023-03-11 16:09:46 +0700

    course nodejs

--- Nodejs/05-url-module.js
+++ Nodejs/05-url-module.js
@@ -1,5 +1,5 @@
 // var url = require("url");
-// var adr = require("http://localhost:8080/default.htm?year=2017&month=february");
+// var adr = require("http://localhost:8080/default.html?year=2017&month=february");
 // var q = url.parse(adr, true);
 
 // console.log(q.host);
@@ -10,22 +10,22 @@
 // console.log(qdata.month);
 
 // file server
-var http = require("http");
-var url = require("url");
-var fs = require("fs");
+// var http = require("http");
+// var url = require("url");
+// var fs = require("fs");
 
-http
-  .createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 not found");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(6060);
+// http
+//   .createServer(function (req, res) {
+//     var q = url.parse(req.url, true);
+//     var filename = "." + q.pathname;
+//     fs.readFile(filename, function (err, data) {
+//       if (err) {
+//         res.writeHead(404, { "Content-Type": "text/html" });
+//         return res.end("404 not found");
+//       }
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     });
+//   })
+//   .listen(6060);
