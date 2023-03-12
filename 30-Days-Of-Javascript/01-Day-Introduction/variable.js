822e937fb621c1c9dc1f9802b8b10bac69f2c7bd
choiruladamm <choiruladamm@gmail.com>
2023-03-12 11:45:25 +0700

    30 days of js

---
+++ 30-Days-Of-Javascript/01-Day-Introduction/variable.js
@@ -0,0 +1,23 @@
+// // Declaring different variables of different data types
+// let firstName = "Asabeneh"; // first name of a person
+// let lastName = "Yetayeh"; // last name of a person
+// let country = "Finland"; // country
+// let city = "Helsinki"; // capital city
+// let age = 100; // age in years
+// let isMarried = true;
+
+// console.log(firstName, lastName, country, city, age, isMarried);
+
+// Declaring variables with number values
+// let age = 100 // age in years
+// const gravity = 9.81 // earth gravity  in m/s2
+// const boilingPoint = 100 // water boiling point, temperature in °C
+// const PI = 3.14 // geometrical constant
+// console.log(gravity, boilingPoint, PI)
+
+const myAge = 21
+const yourAge = 30
+
+console.log(typeof myAge + " " +typeof yourAge)
+console.log(`I am ${myAge} years old`)
+console.log(`You are ${yourAge} years old`)
